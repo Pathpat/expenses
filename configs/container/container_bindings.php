e2f2233b1b347538f5fd8426b2d2c692059f2e91
Pathpat <xristospatakias@gmail.com>
2025-01-31 16:26:59 +0200

    Upload Transaction Receipt files with proper File Validation

--- configs/container/container_bindings.php
+++ configs/container/container_bindings.php
@@ -12,11 +12,14 @@ use App\Csrf;
 use App\DataObjects\SessionConfig;
 use App\Enum\AppEnvironment;
 use App\Enum\SameSite;
+use App\Enum\StorageDriver;
 use App\RequestValidators\RequestValidatorFactory;
 use App\Services\UserProviderService;
 use App\Session;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\ORMSetup;
+use League\Flysystem\Filesystem;
+use League\Flysystem\Local\LocalFilesystemAdapter;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Slim\App;
@@ -102,4 +105,11 @@ return [
         failureHandler: $csrf->failureHandler(),
         persistentTokenMode: true,
     ),
+    Filesystem::class => function (Config $config) {
+        $adapter = match($config->get('storage.driver')) {
+            StorageDriver::Local => new LocalFilesystemAdapter(STORAGE_PATH)
+        };
+
+        return new Filesystem($adapter);
+    },
 ];
