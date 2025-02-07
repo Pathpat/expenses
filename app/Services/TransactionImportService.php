8bd306fccab3d4c6042a2022eb312b11bbb92698
Pathpat <xristospatakias@gmail.com>
2025-02-07 15:02:14 +0200

    Reduce memory consumption and speed up imports

---
+++ app/Services/TransactionImportService.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Services;
+
+use App\DataObjects\TransactionData;
+use App\Entity\Transaction;
+use App\Entity\User;
+use Doctrine\ORM\EntityManager;
+
+class TransactionImportService
+{
+    public function __construct(
+        private readonly CategoryService $categoryService,
+        private readonly TransactionService $transactionService,
+        private readonly EntityManager $entityManager
+    ) {
+    }
+
+    /**
+     * @param  string  $file
+     * @param  User    $user
+     *
+     * @return void
+     * @throws \DateMalformedStringException
+     * @throws \Doctrine\ORM\Exception\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     * @throws \Doctrine\Persistence\Mapping\MappingException
+     */
+    public function importFromFile(string $file, User $user): void
+    {
+        $resource   = fopen($file, 'r');
+        $categories = $this->categoryService->getAllKeyedByName();
+
+        fgetcsv($resource);
+
+        $count     = 1;
+        $batchSize = 250;
+        while (($row = fgetcsv($resource)) !== false) {
+            [$date, $description, $category, $amount] = $row;
+
+            $date     = new \DateTime($date);
+            $category = $categories[strtolower($category)] ?? null;
+            $amount   = str_replace(['$', ','], '', $amount);
+
+            $transactionData = new TransactionData($description, (float) $amount, $date, $category);
+
+            $this->transactionService->create($transactionData, $user);
+
+            if ($count % $batchSize === 0) {
+                $this->entityManager->flush();
+                $this->entityManager->clear(Transaction::class);
+
+                $count = 1;
+            } else {
+                $count++;
+            }
+        }
+
+        if ($count > 1) {
+            $this->entityManager->flush();
+            $this->entityManager->clear();
+        }
+    }
+}
--- app/Services/TransactionService.php
+++ app/Services/TransactionService.php
@@ -112,7 +112,6 @@ class TransactionService
         $transaction->setCategory($transactionData->category);
 
         $this->entityManager->persist($transaction);
-        $this->entityManager->flush();
 
         return $transaction;
     }
